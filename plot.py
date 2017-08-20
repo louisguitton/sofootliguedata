662fb7a35060ffd53f1588abe3036de3a133442c
louisguitton <louisguitton93@gmail.com>
2017-08-20 14:56:31 +0200

    tentative de plot historique

--- plot.py
+++ plot.py
@@ -1,22 +1,38 @@
 import pandas as pd
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
-from bokeh.models import HoverTool
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool, CustomJS, ColumnDataSource, Select
 import datetime
+import os
+
+from bokeh.layouts import gridplot
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
 
-def main():
-    date_element = datetime.date.today().strftime("%Y%m%d")
-    file_path = 'data/sfl_data_{}.csv'.format(date_element)
-    scores = pd.read_csv(file_path)
-
-    output_file("index.html")
-
+def get_source(scores):
     source = ColumnDataSource(data=dict(
         x=scores['Points SFL'],
         y=scores["bet_safety"],
         team=scores["Equipe"],
         game=scores["Match"]
     ))
+    return source
+
+
+def main():
+    files_list = os.listdir('data')
+
+    scores_list = list()
+    for file_path in files_list:
+        scores = pd.read_csv('data/' + file_path)
+        scores_list.append(scores)
+
+    sources_list = list()
+    for scores in scores_list:
+        source = get_source(scores)
+        sources_list.append(source)
+
+    fill_source = get_source(scores_list[0])
 
     hover = HoverTool(
         tooltips=[
@@ -28,14 +44,36 @@ def main():
         names=['teams']
     )
 
+    output_file("index.html")
+
     p = figure(tools=[hover],
-               title=file_path, y_axis_type="log")
+               title="SFL Data", y_axis_type="log")
     p.xaxis.axis_label = "Points SFL"
     p.yaxis.axis_label = "Bet Safety"
 
-    p.scatter('x', 'y', radius=1, source=source, name='teams', alpha=0.8)
+    p.scatter('x', 'y', radius=1, source=fill_source, name='teams', alpha=0.8)
 
-    show(p)
+    select = Select(options=files_list, value=files_list[0])
+
+    codes = """
+    var filename = cb_obj.value;
+    var original_data = fill_source.data;
+    var target_data = filename.data;
+
+    console.log(target_data);
+
+    original_data = target_data
+
+    source.trigger("change");
+    """
+
+    source_dict = {k: v for k, v in zip(files_list, sources_list)}
+
+    select.callback = CustomJS(args=dict(fill_source=fill_source, **source_dict), code=codes)
+
+    grid = gridplot([[select], [p]])
+
+    show(grid)
 
 
 if __name__ == '__main__':
